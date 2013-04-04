9dae2070abf15beaf3094fcc0788615e47dd5169
William Readey <wjreadey@gmail.com>
2013-04-03 22:19:05 -0400

    remove S3

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -70,12 +70,12 @@ Omrails::Application.configure do
   config.action_mailer.default_url_options = { :host => 'localhost:3000' }
 
   # Peperclip should use Amazon S3 on Heroku
-  config.paperclip_defaults = {
-    :storage => :s3,
-    :s3_credentials => {
-        :bucket => ENV['AWS_BUCKET'],
-        :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
-        :secret_access_key => ['AWS_SECRET_ACCESS_KEY']
-      }
-    }
+#  config.paperclip_defaults = {
+#    :storage => :s3,
+#    :s3_credentials => {
+#        :bucket => ENV['AWS_BUCKET'],
+#        :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
+#        :secret_access_key => ['AWS_SECRET_ACCESS_KEY']
+#      }
+#    }
 end
