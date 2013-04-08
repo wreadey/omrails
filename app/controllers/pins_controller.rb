5e0acbfdbb306e53d6351d629363ef54ccc559fe
William Readey <wjreadey@gmail.com>
2013-04-07 22:34:31 -0400

    Add Endless Scroll

--- app/controllers/pins_controller.rb
+++ app/controllers/pins_controller.rb
@@ -10,6 +10,7 @@ class PinsController < ApplicationController
     respond_to do |format|
       format.html # index.html.erb
       format.json { render json: @pins }
+      format.js
     end
   end
 
