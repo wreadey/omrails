deb18003cc3d2b35649046a5ca13438e020a8a6d
William Readey <wjreadey@gmail.com>
2013-04-08 01:31:58 -0400

    Google Static Maps fix for images w no EXIF data

--- app/models/pin.rb
+++ app/models/pin.rb
@@ -23,13 +23,15 @@ class Pin < ActiveRecord::Base
     imgfile = EXIFR::JPEG.new(image.queued_for_write[:original].path)
     return unless imgfile
   
-    lat = imgfile.exif[0].gps_latitude[0].to_f + (imgfile.exif[0].gps_latitude[1].to_f / 60) + (imgfile.exif[0].gps_latitude[2].to_f / 3600)
-    lng = imgfile.exif[0].gps_longitude[0].to_f + (imgfile.exif[0].gps_longitude[1].to_f / 60) + (imgfile.exif[0].gps_longitude[2].to_f / 3600)
+    unless imgfile.gps_latitude.blank? 
+      lat = imgfile.exif[0].gps_latitude[0].to_f + (imgfile.exif[0].gps_latitude[1].to_f / 60) + (imgfile.exif[0].gps_latitude[2].to_f / 3600)
+      lng = imgfile.exif[0].gps_longitude[0].to_f + (imgfile.exif[0].gps_longitude[1].to_f / 60) + (imgfile.exif[0].gps_longitude[2].to_f / 3600)
 
-    lat = lat * -1 if imgfile.exif[0].gps_latitude_ref == "S"      # (N is +, S is -)
-    lng = lng * -1 if imgfile.exif[0].gps_longitude_ref == "W"   # (W is -, E is +)
+      lat = lat * -1 if imgfile.exif[0].gps_latitude_ref == "S"      # (N is +, S is -)
+      lng = lng * -1 if imgfile.exif[0].gps_longitude_ref == "W"   # (W is -, E is +)
 
-    self.img_loc_lat  = lat # imgfile.gps_latitude
-    self.img_loc_lng  = lng # imgfile.gps_longitude
+      self.img_loc_lat  = lat # imgfile.gps_latitude
+      self.img_loc_lng  = lng # imgfile.gps_longitude
+    end
   end
 end
